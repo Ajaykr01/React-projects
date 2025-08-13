316a6294c7b734da5c530d67407f2fcc533fbabb
Ajay <ajaykumar87577@gmail.com>
2025-08-14 01:25:39 +0530

    added some routes

--- 05youtube-clone/src/App.jsx
+++ 05youtube-clone/src/App.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Card from "./components/pages/Card";
 import Layout from "./components/Layout/Layout";
 import SearchedVideos from "./components/pages/SearchedVideos";
+import CategoryPage from "./components/CategoryBar/CategoryPage";
+import Shorts from "./components/pages/Shorts";
+import Subscribers from "./components/pages/Subscribers";
+import You from "./components/pages/You";
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Card />} />
           <Route path="/results" element={<SearchedVideos />} />
+          <Route path="/category/:categoryName" element={<CategoryPage />} />
+          <Route path="/shorts" element={<Shorts />} />
+          <Route path="/subscribers" element={<Subscribers />} />
+          <Route path="/you" element={<You />} />
         </Route>
       </Routes>
     </BrowserRouter>
